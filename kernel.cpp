9f24175482d90bd2ddafd2b561e6afb464b1e965
Arcade Luke <arcadeluke@gmail.com>
2017-11-17 12:17:01 -0500

    Update kernel.cpp
    Completed basic version of kernel.cpp to be made and updated into a complete Operating System.

--- kernel.cpp
+++ kernel.cpp
@@ -1,4 +1,13 @@
-void kernelMain(){
+
+void printf(char* str){
+  unsigned short* VidmeoMemory = (unsigneed short(*)0xb8000;
+                                     
+  for(int i = 0; str[i] != '\0'; ++i)
+      VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
+}
+
+
+extern "C" void kernelMain(void* mutliboot_structure, unsigned int magicnumber){
   printf("Hello world! --- http://AlgorithmMan.de")
     
   while(1):
